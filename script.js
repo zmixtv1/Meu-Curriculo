9dadf2da998375b3f564ccf79815e7d86edd3b14
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-25 18:14:11 -0300

    mudança1.0

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ a.addEventListener("mouseout", sair)
 b.addEventListener("mouseenter", entrar1)
 b.addEventListener("mouseout", sair1)
 function entrar(){
-    a.style.color = "RED"
+    a.style.color = "Blue"
 }
 function sair(){
-    a.style.color = "blue"
+    a.style.color = "rgb(0, 255, 255)"
 }
 function entrar1(){
-    b.style.color = "RED"
+    b.style.color = "Blue"
 }
 function sair1(){
-    b.style.color = "blue"
+    b.style.color = "rgb(0, 255, 255)"
 }
